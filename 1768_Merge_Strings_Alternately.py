e9a45a2425610cec936881c33077f55645189fb2
Divan De Bruin <divan.debruin25@gmail.com>
2024-12-01 17:33:29 -0500

    Update 1768_Merge_Strings_Alternately.py

--- 1768_Merge_Strings_Alternately.py
+++ 1768_Merge_Strings_Alternately.py
@@ -3,36 +3,54 @@
 #______________________
 
 """_summary_
-You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+Problem Description
+You are given two strings, word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If one string is longer than the other, append the additional letters to the end of the merged string.
 
 Return the merged string.
 
- 
+Examples
+Example 1
+Input:
+word1 = "abc"
+word2 = "pqr"
 
-Example 1:
+Output:
+"apbqcr"
 
-Input: word1 = "abc", word2 = "pqr"
-Output: "apbqcr"
-Explanation: The merged string will be merged as so:
-word1:  a   b   c
-word2:    p   q   r
+Explanation:
+The merged string is formed as follows:
+word1:  a   b   c  
+word2:    p   q   r  
 merged: a p b q c r
-Example 2:
 
-Input: word1 = "ab", word2 = "pqrs"
-Output: "apbqrs"
-Explanation: Notice that as word2 is longer, "rs" is appended to the end.
-word1:  a   b 
-word2:    p   q   r   s
+Example 2
+Input:
+word1 = "ab"
+word2 = "pqrs"
+
+Output:
+"apbqrs"
+
+Explanation:
+Since word2 is longer, the remaining characters "rs" are appended to the end:
+word1:  a   b  
+word2:    p   q   r   s  
 merged: a p b q   r   s
-Example 3:
 
-Input: word1 = "abcd", word2 = "pq"
-Output: "apbqcd"
-Explanation: Notice that as word1 is longer, "cd" is appended to the end.
-word1:  a   b   c   d
-word2:    p   q 
+Example 3
+Input:
+word1 = "abcd"
+word2 = "pq"
+
+Output:
+"apbqcd"
+
+Explanation:
+Since word1 is longer, the remaining characters "cd" are appended to the end:
+word1:  a   b   c   d  
+word2:    p   q  
 merged: a p b q c   d
+
 """
 #______________________
 #      SOLUTION
